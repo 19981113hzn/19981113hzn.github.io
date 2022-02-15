7cfb8559c90160c011ff824c66d671f1097ba575
wb.huzenian <wb.huzenian@corp.netease.com>
2022-02-15 15:32:15 +0800

    修改文字

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,8 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
-  descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
-  descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    titleHeader: "翠翠有年年",
+    descriptionHeader: "小翠喜欢年年吗，嫁给年年好吗？^^",
+    buttonYes: "喜欢",
+    buttonNo: "不，不喜欢",
+    titleModar: "我的小翠宝宝",
+    descriptionModar: "非常幸运能够遇上你呀，好想照顾你一辈子呀，世上没有再比你可爱的人了在我心里，昨天是情人节 ，今天是认识的第八个月，我想在这后面加很多个0，来日方长，保持可爱，一起努力哦，我会一直爱你小翠。",
 };

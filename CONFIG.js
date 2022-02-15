62eebe0d9aed4966b3d50f63d5c44ebff25435c9
wb.huzenian <wb.huzenian@corp.netease.com>
2022-02-15 15:49:15 +0800

    修改

--- CONFIG.js
+++ CONFIG.js
@@ -2,7 +2,7 @@ const CONFIGDATA = {
     titleHeader: "翠翠有年年",
     descriptionHeader: "小翠喜欢年年吗，嫁给年年好吗？^^",
     buttonYes: "喜欢",
-    buttonNo: "不，不喜欢",
+    buttonNo: "不，不喜欢[○･｀Д´･ ○]",
     titleModar: "我的小翠宝宝",
     descriptionModar: "非常幸运能够遇上你呀，好想照顾你一辈子呀，世上没有再比你可爱的人了在我心里，昨天是情人节 ，今天是认识的第八个月，我想在这后面加很多个0，来日方长，保持可爱，一起努力哦，我会一直爱你小翠。",
 };
